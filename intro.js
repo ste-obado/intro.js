38dc3fb642a67be32b35eefbb4547699f0b7598d
ste-obado <stephenomoto27@gmail.com>
2026-06-09 15:54:04 +0300

    Update intro.js

--- intro.js
+++ intro.js
@@ -1,3 +1,3 @@
-console.log("Name:John");
-console.log("Age:20");
-console.log("Programming Language:JavaScript")
+console.log("Name:Stephen Oderp");
+console.log("Age:19");
+console.log("Favorite Programming Language:Pyhton")
